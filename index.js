9f41966d8c0000d30d7a36c7835f1e2375e2a853
Zenobius Jiricek <airtonix@gmail.com>
2016-01-28 00:17:31 +1030

    fix merge conflicts

--- index.js
+++ index.js
@@ -9,18 +9,10 @@ var path = require('path');
  */
 var webpack = require('webpack-stream'),
 	debug = require('debug')('gastropod/addons/tasks/webpack'),
-<<<<<<< HEAD
 	named = require('vinyl-named'),
-	plumber = require('gulp-plumber');
-=======
 	accept = require('check-args'),
-	// logging = require('gastropod').Logging
-	named = require('vinyl-named');
->>>>>>> develop
+	plumber = require('gulp-plumber');
 
-var Logging = require('gastropod/src/core/logging'),
-	ErrorHandler = Logging.ErrorHandler,
-	Logger = Logging.Logger;
 
 var buildPath = accept(String, String, String)
 				.to(function(root, staticRoot, section){
@@ -43,14 +35,8 @@ module.exports = function (gulp, gastro){
 	 * Constants
 	 */
 	var Config = gastro.Config,
-		// logger = new logging.Logger('css:scss'),
 
-<<<<<<< HEAD
-	var logger = new Logger('scripts:webpack'),
-	var source = path.join(Config.source.root,
-=======
 		source = buildPath(Config.source.root,
->>>>>>> develop
 						   Config.source.scripts,
 						   Config.filters.scripts.modules),
 
@@ -67,21 +53,10 @@ module.exports = function (gulp, gastro){
 		debug(' > target', target);
 
 		return gulp.src(source)
-			.pipe(logger.incoming())
-			.pipe(plumber(ErrorHandler('Webpack')))
 			.pipe(named())
-			// .pipe(logger.incoming())
-			// .pipe(plumber(logging.ErrorHandler('JS:Webpack')))
 			.pipe(webpack(WebpackConfig))
-<<<<<<< HEAD
-			.pipe(logger.outgoing())
-			.on('error', function handleError(err){ Logger.log(err); this.emit('end'); })
+			.on('error', function handleError(err){ this.emit('end'); })
 			.pipe(gulp.dest(target))
-			.on('end', function(){ Logger.log("Finished"); })
-=======
-			// .pipe(logger.outgoing())
-			.pipe(gulp.dest(target));
->>>>>>> develop
 
 	});
 
